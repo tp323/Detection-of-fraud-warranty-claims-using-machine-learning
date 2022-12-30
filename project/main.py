ae6e614d4ed0255a8b08949ee04a2cd8efce1d18
Tiago Pardal <tiagopardl@hotmail.com>
2022-12-30 02:36:13 +0100

    small fixes, and removed hard coded values more improvements in hard coded values to be done

--- project/main.py
+++ project/main.py
@@ -2,26 +2,27 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-columns_with_strings = ["Area", "Consumer_profile", "Product_category", "Product_type", "Purchased_from", "Purpose"]
 
 
-def treat_vals(data_to_fix):
-    columns_with_values = ["AC_1001_Issue", "AC_1002_Issue", "AC_1003_Issue", "TV_2001_Issue", "TV_2002_Issue",
-                           "TV_2003_Issue", "Claim_Value", "Service_Centre", "Product_Age", "Call_details", "Fraud"]
+def treat_vals(data_to_fix, string_cols):
+    columns_with_numeric_values = list(data_to_fix.select_dtypes(include=[np.number]).columns.values)
     # should Fraud be here to?
-    for i in columns_with_values:
+
+    # fix lower case claim
+    data_to_fix["Purpose"] = data_to_fix["Purpose"].replace("claim", "Claim")
+    for i in columns_with_numeric_values:
         data_to_fix[i] = data_to_fix[i].fillna(data_to_fix[i].mean())
-    for i in columns_with_strings:
+    for i in string_cols:
         data_to_fix[i] = data_to_fix[i].fillna(data_to_fix[i].mode()[0])
 
 
 def one_hot_encode():
     area = ['Rural', 'Urban']
-    consumer_profile = ['Personal', 'Business']
-    product_category = ['Household', 'Entertainment']
-    product_type = ['AC', 'TV']
-    purchased_from = ['Manufacturer', 'Dealer', 'Internet']
-    purpose = ['Other', 'claim', 'Claim', 'Complaint']
+    consumer_profile = ['Business', 'Personal']
+    product_category = ['Entertainment', 'Household']
+    product_type = ['TV', 'AC']
+    purchased_from = ['Manufacturer', 'Internet', 'Dealer']
+    purpose = ['Complaint', 'Other', 'Claim']
     all_arrays = [area, consumer_profile, product_category, product_type, purchased_from, purpose]
     ids = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     cnt = 0
@@ -37,8 +38,9 @@ def one_hot_encode():
 if __name__ == '__main__':
     csv_data = pd.read_csv(r'data\warranty_claims.csv')
     data = csv_data.drop("ID", axis='columns')
-    treat_vals(data)
-
+    columns_with_strings = list(data.select_dtypes(include=['object']).columns)
+    treat_vals(data, columns_with_strings)
+    print(data["Purpose"].value_counts())
     X = data.drop("Fraud", axis='columns')
     y = data['Fraud']
     t = train_test_split(X, y, train_size=.9, test_size=.1)
