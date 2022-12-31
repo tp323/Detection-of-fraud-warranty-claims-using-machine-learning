e0debb0f7cd9d1649e17bb80ab64c5d13a072aea
Tiago Pardal <tiagopardl@hotmail.com>
2022-12-31 02:24:20 +0100

    data treatment for machine learning DONE

--- project/main.py
+++ project/main.py
@@ -1,47 +1,57 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+
+from sklearn.compose import make_column_transformer
+
+UNNECESSARY_COLUMNS = ["ID"]
 
 
-def treat_vals(data_to_fix, string_cols):
-    columns_with_numeric_values = list(data_to_fix.select_dtypes(include=[np.number]).columns.values)
-    # should Fraud be here to?
+def standardize_data(data_to_standardize):
+    scaler = StandardScaler().fit(data_to_standardize)
+    standardized_data = scaler.transform(data_to_standardize)
+    # print(standardized_data)
+    print(standardized_data.std(axis=0))  # proof of standardization
 
-    # fix lower case claim
-    data_to_fix["Purpose"] = data_to_fix["Purpose"].replace("claim", "Claim")
-    for i in columns_with_numeric_values:
-        data_to_fix[i] = data_to_fix[i].fillna(data_to_fix[i].mean())
-    for i in string_cols:
-        data_to_fix[i] = data_to_fix[i].fillna(data_to_fix[i].mode()[0])
-
-
-def one_hot_encode():
-    area = ['Rural', 'Urban']
-    consumer_profile = ['Business', 'Personal']
-    product_category = ['Entertainment', 'Household']
-    product_type = ['TV', 'AC']
-    purchased_from = ['Manufacturer', 'Internet', 'Dealer']
-    purpose = ['Complaint', 'Other', 'Claim']
-    all_arrays = [area, consumer_profile, product_category, product_type, purchased_from, purpose]
-    ids = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    cnt = 0
-    for n in all_arrays:
-        arr = list(zip(ids, n))
-        encoded_array = OneHotEncoder().fit_transform(arr).toarray()
-        print(columns_with_strings[cnt], " - ", arr, ": \n", encoded_array)
-        cnt += 1
-    # TODO(save each encoded array)
-    # TODO(check if its multi column or single approach)
+    return standardized_data
 
 
 if __name__ == '__main__':
-    csv_data = pd.read_csv(r'data\warranty_claims.csv')
-    data = csv_data.drop("ID", axis='columns')
-    columns_with_strings = list(data.select_dtypes(include=['object']).columns)
-    treat_vals(data, columns_with_strings)
-    print(data["Purpose"].value_counts())
+    data = pd.read_csv(r'data\warranty_claims.csv').drop(columns=UNNECESSARY_COLUMNS)
+    data["Purpose"] = data["Purpose"].replace("claim", "Claim")  # fix lower case claim
+
+    num_cols = list(data.select_dtypes(include=[np.number]).columns.values)
+    categorical_cols = list(data.select_dtypes(include=['object']).columns)
+    x_numeric_cols = list(data.select_dtypes(include=['float64']).columns.values)  # all numeric except Fraud
+    y_col = ['Fraud']
+
+    ohe = OneHotEncoder()
+    si = SimpleImputer()
+
+    # column transformers
+    # ct_all = make_column_transformer((ohe, categorical_cols), (si, num_cols), remainder='passthrough')
+    ct_x = make_column_transformer((ohe, categorical_cols), (si, x_numeric_cols), remainder='passthrough')
+
+    # one hot encode and fill missing values
+    # encoded_data = ct_all.fit_transform(data)
+
     X = data.drop("Fraud", axis='columns')
-    y = data['Fraud']
-    t = train_test_split(X, y, train_size=.9, test_size=.1)
-    one_hot_encode()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.9, test_size=.1)
+
+    # one hot encode and fill missing values
+    X_train = ct_x.fit_transform(X_train)
+    X_test = ct_x.fit_transform(X_test)
+
+    # standardize data
+    # no need to standardize y since its binary (1.0, 0.0)
+    X_train_scaled = standardize_data(X_train)
+    X_test_scaled = standardize_data(X_test)
+
+    # TODO(CHECK IF MORE TREATMENT OF DATA IS REQUIRED FOR Y)
+    # print(X_train_scaled)
+    # print(X_test_scaled)
--- project/utils.py
+++ project/utils.py
@@ -1,43 +1,9 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from main import *
-
-columns = ["Area", "Consumer_profile", "Product_category", "Product_type", "AC_1001_Issue", "AC_1002_Issue",
-               "AC_1003_Issue", "TV_2001_Issue", "TV_2002_Issue", "TV_2003_Issue", "Claim_Value", "Service_Centre",
-               "Product_Age", "Purchased_from", "Call_details", "Purpose", "Fraud"]
-
-
-def find_domain(column):
-    unique_values = set()
-    for element in column:
-        unique_values.add(element)
-    return list(unique_values)
 
 
 if __name__ == '__main__':
-    csv_data = pd.read_csv(r'data\warranty_claims.csv')
-    data = csv_data.drop("ID", axis='columns')
-    columns_with_numeric_values = list(data.select_dtypes(include=[np.number]).columns.values)
-    columns_with_strings = list(data.select_dtypes(include=['object']).columns)
-    columns = list(data.columns.values)
+    print()
 
-    treat_vals(data, columns_with_strings)
-
-    # print(list(data.select_dtypes(include=['int64']).columns.values))  # Fraud column
-    # print(list(data.select_dtypes(include=['float64']).columns.values))  # all number columns except Fraud
-
-    for n in columns_with_strings:
-        print(n, "=", find_domain(data[n]))
-
-# Unique values
-# Area = ['Rural', 'Urban']
-# Consumer_profile = ['Business', 'Personal']
-# Product_category = ['Entertainment', 'Household']
-# Product_type = ['TV', 'AC']
-# Purchased_from = ['Manufacturer', 'Internet', 'Dealer']
-# Purpose = ['Complaint', 'Other', 'Claim']
-
-# Area,Consumer_profile,Product_category,Product_type,AC_1001_Issue,AC_1002_Issue,AC_1003_Issue,TV_2001_Issue,TV_2002_Issue,TV_2003_Issue,Claim_Value,Service_Centre,Product_Age,Purchased_from,Call_details,Purpose,Fraud
-# AC_1001_Issue,AC_1002_Issue,AC_1003_Issue,TV_2001_Issue,TV_2002_Issue,TV_2003_Issue,Claim_Value,Service_Centre,Product_Age,Call_details
-# Area,Consumer_profile,Product_category,Product_type,Purchased_from,Purpose
+# column transformers
+# ct_all = make_column_transformer((ohe, categorical_cols), (si, num_cols), remainder='passthrough')
+# ct_x = make_column_transformer((ohe, categorical_cols), (si, x_numeric_cols), remainder='passthrough')
+# ct_y = make_column_transformer((si, y_col), remainder='passthrough')
