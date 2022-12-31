a88dbd269aa676c9d485238d174f95341b161d86
Tiago Pardal <tiagopardl@hotmail.com>
2022-12-31 02:28:15 +0100

    fix to last commit

--- project/main.py
+++ project/main.py
@@ -27,19 +27,19 @@ if __name__ == '__main__':
     num_cols = list(data.select_dtypes(include=[np.number]).columns.values)
     categorical_cols = list(data.select_dtypes(include=['object']).columns)
     x_numeric_cols = list(data.select_dtypes(include=['float64']).columns.values)  # all numeric except Fraud
-    y_col = ['Fraud']
+    y_col = ['Fraud']  # Fraud column
 
     ohe = OneHotEncoder()
     si = SimpleImputer()
 
     # column transformers
-    # ct_all = make_column_transformer((ohe, categorical_cols), (si, num_cols), remainder='passthrough')
     ct_x = make_column_transformer((ohe, categorical_cols), (si, x_numeric_cols), remainder='passthrough')
 
     # one hot encode and fill missing values
     # encoded_data = ct_all.fit_transform(data)
 
     X = data.drop("Fraud", axis='columns')
+    y = data['Fraud']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.9, test_size=.1)
 
--- project/utils.py
+++ project/utils.py
@@ -3,6 +3,11 @@
 if __name__ == '__main__':
     print()
 
+
+# num_cols = list(data.select_dtypes(include=[np.number]).columns.values)
+# categorical_cols = list(data.select_dtypes(include=['object']).columns)
+# x_numeric_cols = list(data.select_dtypes(include=['float64']).columns.values)  # all numeric except Fraud
+
 # column transformers
 # ct_all = make_column_transformer((ohe, categorical_cols), (si, num_cols), remainder='passthrough')
 # ct_x = make_column_transformer((ohe, categorical_cols), (si, x_numeric_cols), remainder='passthrough')
