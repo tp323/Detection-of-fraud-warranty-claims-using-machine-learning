17c218a5cb7835ac4c6b99705b239ed11bbe3691
Tiago Pardal <tiagopardl@hotmail.com>
2023-01-02 03:23:29 +0100

    classification report

--- project/main.py
+++ project/main.py
@@ -1,5 +1,6 @@
 from mlp import mlp_predicting
 from data_treatment import read_and_treat_data
+from classification_report import classify_ml_solution
 from sklearn.metrics import accuracy_score
 
 
@@ -8,3 +9,4 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = read_and_treat_data()
     predicted_data = mlp_predicting(X_train, X_test, y_train)
     print("Accuracy = ", accuracy_score(y_test, predicted_data))
+    classify_ml_solution(y_test, predicted_data)
